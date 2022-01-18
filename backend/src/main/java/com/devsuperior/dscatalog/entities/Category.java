8bc886d930690745e4d689992b814dafe0d9d440
fcandiotti <fernando_candiotti@hotmail.com>
2022-01-18 07:18:55 -0300

    CategoryResource

---
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -0,0 +1,59 @@
+package com.devsuperior.dscatalog.entities;
+
+import java.io.Serializable;
+
+public class Category implements Serializable {
+    private static final long SerialVersionUID = 1;
+
+    private Long id;
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result * ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if(getClass() != obj.getClass())
+            return false;
+        Category category = (Category) obj;
+        if  (id == null) {
+            return false;
+        } else if (!id.equals(category.id))
+            return false;
+        return true;
+    }
+
+}
